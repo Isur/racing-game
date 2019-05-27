1ebb22ca4e7642a484d91fdeee7caeedbd3254f8
dgrajewski <dawids115@gmail.com>
2019-05-27 20:38:28 +0200

    Added engine sound;

---
+++ Assets/Scripts/CarAudio.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CarAudio : MonoBehaviour
+{
+    public AudioClip EngineSound;
+
+    private AudioSource EngineAudioSource { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EngineAudioSource = gameObject.AddComponent<AudioSource>();
+        EngineAudioSource.clip = EngineSound;
+        EngineAudioSource.loop = true;
+        EngineAudioSource.playOnAwake = true;
+        EngineAudioSource.Play();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void SetEngineSoundPitch(float value)
+    {
+        EngineAudioSource.pitch = value;
+    }
+}
