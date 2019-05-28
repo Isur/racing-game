ea91c1439a2fed6ab4cccbab63a094cc90cec000
Isur <bednarczykartur96@gmail.com>
2019-05-29 01:14:31 +0200

    menu with sound and blocked spam buttons

--- Assets/Scripts/Menu/MenuButton.cs
+++ Assets/Scripts/Menu/MenuButton.cs
@@ -8,19 +8,39 @@ public class MenuButton : MonoBehaviour
     [SerializeField] Animator animator;
     [SerializeField] AnimatorFunctions animatorFunctions;
     [SerializeField] int thisIndex;
+    private bool _wait = false;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void buttonPressed(int button){
+        _wait = true;
+        switch(button){
+            case 0:
+                Debug.Log("PLAY");
+                break;
+            case 1:
+                Debug.Log("STORY");
+                break;
+            case 2:
+                Debug.Log("OPTIONS");
+                break;
+            case 3:
+                Debug.Log("EXIT");
+                break;
+        }
+        StartCoroutine(test());
+    }
     // Update is called once per frame
     void Update()
     {
         if(menuButtonController.index == thisIndex){
             animator.SetBool("selected", true);
-            if(Input.GetAxis("Submit") == 1){
+            if(Input.GetAxis("Submit") == 1 && !_wait){
                 animator.SetBool("pressed", true);
+                buttonPressed(thisIndex);
             } else if(animator.GetBool("pressed")){
                 animator.SetBool("pressed", false);
                 animatorFunctions.disableOnce = true;
@@ -29,4 +49,9 @@ public class MenuButton : MonoBehaviour
             animator.SetBool("selected", false);
         }
     }
+    IEnumerator test(){
+        yield return new WaitForSeconds(0.3f);
+        _wait = false;
+    }
 }
+
