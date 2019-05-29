4c4ed582028919dd8bbd54a4d0701cd3e1c876db
Artur Bednarczyk <bednarczykartur96@gmail.com>
2019-05-29 21:48:20 +0200

    Main menu (#3)
    * Added engine sound;
    
    * main-menu: background, buttons
    
    * Added sound for collision;
    
    * Added random music player;
    
    * Added controller with sounds;
    
    * main menu things
    
    * menu with sound and blocked spam buttons
    
    * Menu scene transition to another scene

---
+++ Assets/Scripts/Menu/MenuButton.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    [SerializeField] MenuButtonController menuButtonController;
+    [SerializeField] Animator animator;
+    [SerializeField] AnimatorFunctions animatorFunctions;
+    [SerializeField] int thisIndex;
+    private bool _wait = false;
+
+    void buttonPressed(int button){
+        _wait = true;
+        switch(button){
+            case 0:
+                Debug.Log("PLAY");
+                Initiate.Fade("TrainingScene",Color.black,1.0f);
+                break;
+            case 1:
+                Debug.Log("STORY");
+                break;
+            case 2:
+                Debug.Log("OPTIONS");
+                break;
+            case 3:
+                Debug.Log("EXIT");
+                break;
+        }
+        StartCoroutine(test());
+    }
+    void Update()
+    {
+        if(menuButtonController.index == thisIndex){
+            animator.SetBool("selected", true);
+            if(Input.GetAxis("Submit") == 1 && !_wait){
+                animator.SetBool("pressed", true);
+                buttonPressed(thisIndex);
+            } else if(animator.GetBool("pressed")){
+                animator.SetBool("pressed", false);
+                animatorFunctions.disableOnce = true;
+            }
+        } else {
+            animator.SetBool("selected", false);
+        }
+    }
+    IEnumerator test(){
+        yield return new WaitForSeconds(0.3f);
+        _wait = false;
+    }
+}
+
