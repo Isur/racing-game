9381b4da102ae63021702b5b229cbbe1cf3fd98d
Artur Bednarczyk <bednarczykartur96@gmail.com>
2019-05-30 18:20:15 +0200

    Cleanup - project structure (#4)
    * Basic race logic;
    
    * Finish message;
    
    * Added finish line;
    
    * fix;
    
    * Bugif of wrong finish line;
    
    * gitignore update
    
    * Project folder structure, gitignore update
    
    * race-login-newStructure

---
+++ Assets/Scripts/Race/Checkpoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private List<Transform> Checkpoints { get; set; } = new List<Transform>();
+    public int CheckpointsCount { get { return Checkpoints.Count; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform t in transform)
+        {
+            Checkpoints.Add(t);
+            t.gameObject.GetComponent<Renderer>().enabled = false;
+        }
+    }
+
+    public Transform GetNext(Transform current)
+    {
+        var currentIndex = Checkpoints.IndexOf(current);
+
+        if (currentIndex == Checkpoints.Count - 1)
+            return Checkpoints[0];
+
+        return Checkpoints[currentIndex + 1];
+    }
+}
---
+++ Assets/Scripts/Utils/NumberEnding.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class NumberEnding
+{
+    public string Calculate(int number)
+    {
+        string suff = string.Empty;
+
+        var ones = number % 10;
+        var tens = Math.Floor(number / 10m) % 10;
+        if (tens == 1)
+        {
+            suff = "th";
+        }
+        else
+        {
+            switch (ones)
+            {
+                case 1: suff = "st"; break;
+                case 2: suff = "nd"; break;
+                case 3: suff = "rd"; break;
+                default: suff = "th"; break;
+            }
+        }
+        return suff;
+    }
+}
