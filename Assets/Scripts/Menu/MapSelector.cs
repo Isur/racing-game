c141f84545e3fe211e46878737294be03bac6ffe
Artur Bednarczyk <bednarczykartur96@gmail.com>
2019-06-01 19:32:08 +0200

    Barrel, map selector, story (#8)

---
+++ Assets/Scripts/Menu/MapSelector.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapSelector : MonoBehaviour
+{
+    public KeyCode  nextMap = KeyCode.RightArrow,
+                    prevMap = KeyCode.LeftArrow,
+                    submit = KeyCode.Return,
+                    esc = KeyCode.Escape;
+    [SerializeField]
+    Animation animationClick;
+    [SerializeField]
+    int thisIndex;
+    
+
+    public AudioSource Audio;
+    public AudioClip ClickSound;
+    public Image Slider;
+    public List<Image> RaceImages = new List<Image>();
+    public List<string> RaceNames = new List<string>();
+
+    private bool _wait = false;
+    private int _currentScene = 0;
+
+    void Start() {
+        Slider.sprite = RaceImages[0].sprite;   
+    }
+    void Update()
+    {
+        if(!_wait && Input.GetKeyDown(nextMap)){
+            ChangeMap(1);
+        } else if(!_wait && Input.GetKeyDown(prevMap)){
+            ChangeMap(0);
+        } else if (Input.GetKeyDown(submit)){
+            Initiate.Fade(RaceNames[_currentScene], Color.black, 1.0f);
+        } else if (Input.GetKeyDown(esc)){
+            Initiate.Fade("MainMenu", Color.black, 1.0f);
+        }
+    }
+
+    private void ChangeMap(int dir){
+         _wait = true;
+         Audio.PlayOneShot(ClickSound);
+        if(thisIndex == dir) animationClick.Play("Click");
+        if(dir == 1){
+            if(_currentScene + 1 < RaceNames.Count){
+                _currentScene++;
+            } else {
+                _currentScene = 0;
+            }
+        } else if (dir == 0){
+            if(_currentScene > 0){
+                _currentScene--;
+            } else {
+                _currentScene = RaceNames.Count - 1;
+            }
+        }
+        Slider.sprite = RaceImages[_currentScene].sprite;
+        StartCoroutine(buttonClick());
+    }
+
+    IEnumerator buttonClick(){
+        yield return new WaitForSeconds(0.3f);
+        _wait = false;
+    }
+}
---
+++ Assets/Scripts/Menu/StoryScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryScript : MonoBehaviour
+{
+    private KeyCode esc = KeyCode.Escape;
+    void Update()
+    {
+        if(Input.GetKeyDown(esc)){
+            Initiate.Fade("MainMenu", Color.black, 1.0f);
+        }
+    }
+}
